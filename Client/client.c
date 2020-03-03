3f95e365194cfb553060aea90fd0ac29a5ff0a44
andreology27 <37252145+andreology@users.noreply.github.com>
2020-03-03 14:04:38 -0800

    working

--- Client/client.c
+++ Client/client.c
@@ -14,34 +14,38 @@
 #include <arpa/inet.h>
 #include <dirent.h>
 
-#define sendrecvflag 0 
+#define sendrecvflag 0
 
 
-int assign_sock_addy(struct sockaddr_in addy, int port, char ip) {
-  int validate = 1;
-
-  return validate;
+//helper functions
+//From FileTransfer Examplse
+void clearBuf(char* b)
+{
+	int i;
+	for (i = 0; i < 32; i++)
+		b[i] = '\0';
 }
-
-void socket_descriptor(int socket) {
-  //taken from professor Fahim code.
-  if (socket < 0)
-		printf("\nfile descriptor not received!!\n");
-	else
-		printf("\nfile descriptor %d received\n", socket);
+int sendFile(FILE* fp, char* buf, int s)
+{
+		char ch;
+		for (int i = 0; i < s; i++) {
+			ch = fgetc(fp);
+			buf[i] = ch;
+			if (ch == EOF)
+				return 1;
+		}
+		return 0;
 }
-
 int main(int argc, char *argv[]) {
   char *ip_address[20];
   int port;
   struct sockaddr_in socket_connect;
   char client_buffer[32];
+  //reading input for ip address and port
   sscanf(argv[1], "%s", &ip_address);
   sscanf(argv[2], "%d", &port);
   int sc_length = sizeof(socket_connect);
   FILE* fp;
-  int nbytes;
- 
   //represents an address family, where most internet apps use AF_INET
   socket_connect.sin_family = AF_INET;
   //a 16-bit port number in Network Byte order
@@ -51,84 +55,79 @@ int main(int argc, char *argv[]) {
 
   //creating socket for client
   int client_socket = socket(AF_INET, SOCK_DGRAM, 0);
-  socket_descriptor(client_socket);
   //connecting to server
   if (connect(client_socket, (struct sockaddr *)&socket_connect, sizeof(socket_connect)) < 0)
   {
     printf("\nConnection Failed \n");
     return -1;
   }else {
-    printf("\nConnection established\n");
+    printf("\n>>> Connection established\n");
   }
   char dir[] = "dir";
-  send(client_socket , "Connected!" , strlen("Connected!") , 0 );
-  printf("IP: %s Port: %d\n", ip_address, port);
+  char con_mess[] = ">>> Connected!\n";
+  send(client_socket , con_mess , strlen(con_mess) , 0 );
+  printf("\nIP: %s Port: %d\n", ip_address, port);
+
   while(1)
   {
+		clearBuf(client_buffer);
     printf("\nEnter a valid file name on the server or enter “dir” command:\n");
-   
     scanf("%s",client_buffer); //user enters value
-    
     fp = fopen(client_buffer, "r");
     //File is not in directory
-    if(fp == NULL && strcmp(client_buffer, dir) != 0)
+    if(fp == NULL )
     {
       printf("\nFile not found\n");
-    }
-     /*------------------Req 6------------------*/
-    else if(fp == NULL && strcmp(client_buffer, dir) == 0){
-      //  sendto(client_socket, client_buffer, 32, 
-      //             sendrecvflag, (struct sockaddr*)&socket_connect, sizeof(socket_connect));
-       write(client_socket, client_buffer,sizeof(client_buffer));
-       bzero(client_buffer,sizeof(client_buffer));
-       
-       //Doesn't Work v
-       recvfrom(client_socket, client_buffer, 32, 
-                    0, (struct sockaddr*)&socket_connect, &sc_length);
-       //Doesn't Work ^
-       
-       printf("\ndirectories:\n");
-       printf("%s", client_buffer);
-       bzero(client_buffer,sizeof(client_buffer));
-    }
-     /*------------------Req 6------------------*/
-
-     /*------------------Req 5------------------*/
-    //User sends a file that is in the directory
-    else
-    {
-      //Send File needs to be added
-      sendto(client_socket, client_buffer, 32, 
-                  sendrecvflag, (struct sockaddr*)&socket_connect, sizeof(socket_connect));
-      printf("\nFile successfully sent\n");
-    }
-    /*------------------Req 5------------------*/
-    
-    printf("\n-------------\n");
- 
-    //while(1){}
-  }
-
+    }else {
+			printf("\nsending file contnet\n");
+			send(client_socket , client_buffer , strlen(client_buffer) , 0 );
+				printf("\nIn else \n");
+			//send(client_socket , client_buffer , strlen(client_buffer) , 0 );
+			while(1) {
+				printf("\n2nd while \n");
+				clearBuf(client_buffer);
+				read(server_socket, server_buffer, sizeof(server_buffer));
+					clearBuf(client_buffer);
+				int y = recvfrom(client_socket, client_buffer, 32,
+									0, (struct sockaddr*)&socket_connect,
+									&sc_length);					printf("%s\n", client_buffer);
+					printf("\n%s\n", client_buffer);
+					// process
+					// if (sendFile(fp, client_buffer, 32)) {
+					// 	send(client_socket , client_buffer , strlen(client_buffer) , 0 );
+					// 	break;
+					// }
+					// printf("\nclosing file \n");
+					// fclose(fp);
+				}
+		}
+  //    /*------------------Req 6------------------*/
+  //   else if(fp == NULL && strcmp(client_buffer, dir) == 0){
+	//
+  //      write(client_socket, client_buffer,sizeof(client_buffer));
+  //      bzero(client_buffer,sizeof(client_buffer));
+	//
+  //      //Doesn't Work v
+  //      recvfrom(client_socket, client_buffer, 32,
+  //                   0, (struct sockaddr*)&socket_connect, &sc_length);
+	//
+  //      printf("\ndirectories:\n");
+  //      printf("%s", client_buffer);
+  //      bzero(client_buffer,sizeof(client_buffer));
+  //   }
+  //    /*------------------Req 5------------------*/
+  //   //User sends a file that is in the directory
+  //   else
+  //   {
+  //     //Send File needs to be added
+  //     sendto(client_socket, client_buffer, 32,
+  //                 sendrecvflag, (struct sockaddr*)&socket_connect, sizeof(socket_connect));
+  //     printf("\nFile successfully sent\n");
+  //   }
+  //   printf("\n-------------\n");
+	//
+   }
   /*------------------Req 5------------------*/
   //Taken from FileTransfer example
-  int sendFile(FILE* fp, char* buf, int s) 
-  {  
-    // if (fp == NULL) { 
-    // 	strcpy(buf, nofile); 
-    // 	int len = strlen(nofile); 
-    // 	buf[len] = EOF; 
-    // 	//for (i = 0; i <= len; i++) 
-    // 		//buf[i] = Cipher(buf[i]); 
-    //   return 1; 
-    // } 
-    char ch; 
-    for (int i = 0; i < s; i++) { 
-      ch = fgetc(fp);  
-      buf[i] = ch; 
-      if (ch == EOF) 
-        return 1; 
-    } 
-    return 0; 
-  }
-/*------------------Req 5------------------*/
+
 }
--- server.c
+++ server.c
@@ -13,35 +13,40 @@
 #include <arpa/inet.h>
 #include <dirent.h>
 
+typedef enum {true, false} bool;
 //helper functions
 //From FileTransfer Examplse
-void clearBuf(char* b) 
-{ 
-	int i; 
-	for (i = 0; i < 32; i++) 
-		b[i] = '\0'; 
+void clearBuf(char* b)
+{
+	int i;
+	for (i = 0; i < 32; i++)
+		b[i] = '\0';
+}
+/*------------------Req 5------------------*/
+//Taken from FileTransfer Example
+int recvFile(char* buf, int s)
+{
+	char ch;
+	for (int i = 0; i < s; i++) {
+		ch = buf[i];
+		if (ch == EOF)
+			return 1;
+		else
+			printf("%c", ch);
+	}
+	return 0;
 }
 
-
-void verify_sock_descriptor(int socket) {
-  //verifying descripto
-  if (socket < 0)
-		printf("\nERROR: Socket not established\n");
-	else
-		printf("\nSocket established w/descriptor %d \n", socket);
-}
-
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
   int port;
   int new_socket;
   int socket_descr;
-  int incoming_socket;
   char server_buffer[32];
   char dir[] = "dir";
   FILE* fp;
-  struct dirent *de;  // Pointer for directory entry 
-
+  struct dirent *de;  // Pointer for directory entry
+	//reading input for port
   sscanf(argv[1], "%d", &port);
   //creating socket structuresockaddr_in
   struct sockaddr_in socket_connect;
@@ -54,85 +59,76 @@ int main(int argc, char *argv[])
   //printf("stuff in args %d\n", port);
   //creating socket
   int server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-  verify_sock_descriptor(server_socket);
 
+	char touch_command[] = "touch";
+
+	//strcat(touch_command, " tsafd.txt");
+	//system(touch_command);
   //binding to socket at address and port number specified
-  if (bind(server_socket, (struct sockaddr*)&socket_connect, sizeof(socket_connect)) == 0)
-    printf("\nSuccessfully binded!\n");
-  else
+  if (bind(server_socket, (struct sockaddr*)&socket_connect, sizeof(socket_connect)) != 0)
     printf("\nBinding Failed!\n");
+
   read(server_socket, server_buffer, sizeof(server_buffer));
   printf("\n%s\n", server_buffer);
   clearBuf(server_buffer);
   printf("\n%s\n", server_buffer);
-  
- while(1) 
- {
-    bzero(server_buffer,sizeof(server_buffer));
-    recvfrom(server_socket, server_buffer, 32, 0, (struct sockaddr*)&socket_connect, &sc_length);
-    printf("recvfrom: %s\n",server_buffer);
-    
-    /*------------------Req 6------------------*/
-    if(strcmp(server_buffer, dir) == 0)
-    {
-      // opendir() returns a pointer of DIR type.  
-      DIR *dr = opendir(".");
-      char temp[32];
-      int first = 0;
-      while ((de = readdir(dr)) != NULL) 
-      {
-        if(first == 0){
-          strcpy(temp,de->d_name);
-          strcat(temp, "\n");
-          first = 1;
-        }
-        else
-        {
-          //printf("%s", de->d_name); 
-          strcat(temp,de->d_name);
-          strcat(temp, "\n");
-        }
-      }
-      bzero(server_buffer,sizeof(server_buffer));      
-      strcpy(server_buffer,temp);
-      //printf("2. %s\n",server_buffer);
-      write(server_socket,server_buffer,sizeof(server_buffer));            
-    }
-    /*------------------Req 6------------------*/
-    
-    /*------------------Req 5------------------*/
-    else
-    {
-      fp = fopen(server_buffer, "r");
-      if(fp == NULL){
-        //receieve file from client
-      }
-      else{
-        //Send "File Already Exists" message to client
-      }
-    }
-    /*------------------Req 5------------------*/
-     //printf("\nafter:\n%s", server_buffer);
-     //printf("\nsize: %ld\n", sizeof(server_buffer));
 
-   
- }
+
+
+	 while(1)
+	 {
+		 printf("\nserver whil \n");
+    //bzero(server_buffer,sizeof(server_buffer));
+		clearBuf(server_buffer);
+		read(server_socket, server_buffer, sizeof(server_buffer));
+    printf("chceking %s\n", server_buffer);
+		bool flag = false;
+		fp = fopen(server_buffer, "r");
+			if(fp == NULL)
+			{
+				clearBuf(server_buffer);
+				printf("\nohting\n");
+				char con_mess[] = "no";
+				send(server_socket , con_mess , strlen(con_mess) , 0 );
+
+				clearBuf(server_buffer);
+				while(1) {
+					printf("server 2nd while");
+				recvfrom(server_socket, server_buffer, 32,
+								0, (struct sockaddr*)&socket_connect,
+								&sc_length);
+
+ 							}
+			}
+
+
+		printf("cleating buffer");
+		 clearBuf(server_buffer);
+		 // if (fp != NULL)
+ 			// fclose(fp);
+ 		}
   return 0;
 }
-
-/*------------------Req 5------------------*/
-//Taken from FileTransfer Example
-int recvFile(char* buf, int s) 
-{ 
-	char ch; 
-	for (int i = 0; i < s; i++) { 
-		ch = buf[i]; 
-	//	ch = Cipher(ch); 
-		if (ch == EOF) 
-			return 1; 
-		else
-			printf("%c", ch); 
-	} 
-	return 0; 
-} 
-/*------------------Req 5------------------*/
+// /*------------------Req 6------------------*/
+// if(strcmp(server_buffer, dir) == 0)
+// {
+//   DIR *dr = opendir(".");
+//   char temp[32];
+//   int first = 0;
+//   while ((de = readdir(dr)) != NULL)
+//   {
+//     if(first == 0){
+//       strcpy(temp,de->d_name);
+//       strcat(temp, "\n");
+//       first = 1;
+//     }
+//     else
+//     {
+//       strcat(temp,de->d_name);
+//       strcat(temp, "\n");
+//     }
+//   }
+//   bzero(server_buffer,sizeof(server_buffer));
+//   strcpy(server_buffer,temp);
+//   write(server_socket,server_buffer,sizeof(server_buffer));
+// }
